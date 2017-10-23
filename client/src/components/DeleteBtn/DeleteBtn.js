04596022de89e2340efb3bd6f857a1bef1cb09ab
Daniel Ascher <ca55per@gmail.com>
2017-10-22 20:00:31 -0400

    Fixing Some Overlaps

--- client/src/components/DeleteBtn/DeleteBtn.js
+++ client/src/components/DeleteBtn/DeleteBtn.js
@@ -1,12 +1,7 @@
 import React from "react";
-import "./DeleteBtn.css";
 
-// The ...props means, spread all of the passed props onto this element
-// That way we don't have to define them all individually
-const DeleteBtn = props => (
-  <span className="delete-btn" {...props}>
-    ✗
-  </span>
+const DeleteBtn = () => (
+  <button className="btn btn-warning" style={{ fontFamily: "Lato" }}>Delete this Budget Item</button>
 );
 
 export default DeleteBtn;
--- client/src/pages/example/Example.js
+++ client/src/pages/example/Example.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
 import Nav from "../../components/Nav";
+//import ProgBar from "../../components/ProgressBar"; <ProgBar />
+//import Input from "../../components/InputForm"; <Input />
+//import AddBtn from "../../components/AddBtn"; <AddBtn />
+//import DeleteBtn from "../../components/DeleteBtn"; <DeleteBtn />
+import FlatNav from "../../components/FlatNav";
+import Login from "../../components/Login";
+import BudgetItem from "../../components/BudgetItem";
+
 
 class Example extends Component {
   render() {
@@ -10,6 +18,12 @@ class Example extends Component {
       <Jumbotron value="This is commented out!">
         <h1 style={{ color: "white", textAlign: "center", lineHeight: "150px" }}>Budge It!</h1>
       </Jumbotron>
+      <FlatNav />
+      <Login />
+      <BudgetItem />
+      <BudgetItem />
+      <BudgetItem />
+      <BudgetItem />
       </div>
     );
   }
