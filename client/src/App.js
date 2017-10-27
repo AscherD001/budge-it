d13d4e6514417f58d43bb2ff0898777a8bb481a8
Daniel Ascher <ca55per@gmail.com>
2017-10-27 18:53:58 -0400

    Merged AuthO to Project Files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,76 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navpills from "./components/Navpills";
 import Homepage from "./pages/Homepage";
 import Budget from "./pages/Budget";
 
-// import Books from "./pages/Books";
-// import Nav from "./components/Nav";
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
 
-const App = () => 
-  <Router>
-    <div>
-      <Navpills />
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/Budget" component={Budget} />
-    </div>
-  </Router> ;
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+          </Navbar.Header>
+        </Navbar>
+        <Router>
+          <div>
+            <Navpills />
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/Budget" component={Budget} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
-
-//<Budget /> inside div to display budget page.
-
-// <Route exact path="/Bills" component={Bills} />
