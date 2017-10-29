b5255bf46c703791d9467f3ee1c27b29e553815b
Gamec0ck4l <theelongnguyen@gmail.com>
2017-10-28 23:29:56 -0400

    Integrated Login to the Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -25,22 +25,9 @@ class App extends Component {
 
     return (
       <div>
-        <Navbar fluid>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">Auth0 - React</a>
-            </Navbar.Brand>
-            <Button
-              bsStyle="primary"
-              className="btn-margin"
-              onClick={this.goTo.bind(this, 'home')}
-            >
-              Home
-            </Button>
             {
               !isAuthenticated() && (
                   <Button
-                    bsStyle="primary"
                     className="btn-margin"
                     onClick={this.login.bind(this)}
                   >
@@ -51,7 +38,6 @@ class App extends Component {
             {
               isAuthenticated() && (
                   <Button
-                    bsStyle="primary"
                     className="btn-margin"
                     onClick={this.logout.bind(this)}
                   >
@@ -59,9 +45,7 @@ class App extends Component {
                   </Button>
                 )
             }
-          </Navbar.Header>
-        </Navbar>
-        <Router>
+            <Router>
           <div>
             <Navpills />
             <Route exact path="/" component={Homepage} />
