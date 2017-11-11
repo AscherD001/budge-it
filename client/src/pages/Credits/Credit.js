68639cd4076aac15511ec5b26f2a2877ce2d4f23
buikevin96@gmail.com <buikevin96@gmail.com>
2017-11-11 11:37:05 -0500

    Updated Credits

--- client/src/pages/Credits/Credit.js
+++ client/src/pages/Credits/Credit.js
@@ -16,31 +16,31 @@ class Credit extends Component {
                 <div className="ascher">
                     <img src="https://avatars0.githubusercontent.com/u/28353831?s=400&v=4" alt="Daniel Ascher" height="200" width="200" class="ashcer"/>
                     <a href="https://github.com/Wicked001"> Daniel Ascher </a>
-                    <span className="ascher"> Daniel was a member of the may 2017 coding boot camp cohort. he has a passion for video game design and is currently seeking employment. </span>
+                    <span className="ascher"> Daniel loves working with others and hopes to work in a magical land field with gummy bears, English, and has a strong foundation with Javascript. </span>
                 </div>
 
                 <div className="mcgeough">
                     <img src="https://avatars1.githubusercontent.com/u/26332545?s=400&v=4" alt="Daniel McGeough" height="200" width="200" id="mcgeough"/>
                     <a href="https://github.com/Dmcgeough"> Daniel McGeough Jr. </a>
-                    <span className="mcgeough"> Daniel was a member of the may 2017 coding boot camp cohort. he has a passion for video game design and is currently seeking employment.</span>
+                    <span className="mcgeough"> Daniel is from England and likes to work with VanillaJS, SQLite, Bootstrap Framework. His passions include video games and writing code.</span>
                 </div>
 
                 <div className="nguyen">
                     <img src="https://avatars3.githubusercontent.com/u/26380040?s=400&v=4" alt="Long Nguyen" height="200" width="200" id="nguyen"/>
                     <a href="https://github.com/Gamec0ck4l"> Long Nguyen </a>
-                    <span className="nguyen"> Long was a member of the may 2017 coding boot camp cohort. he has a passion for video game design and is currently seeking employment. </span>
+                    <span className="nguyen"> Long works well with HTML, CSS, Javascript, and all things backend. His favorite hobbies include trading and being with his family. </span>
                 </div>
 
                 <div className="hopkins">
                     <img src={require(`./hopkins.png`)} alt="Stephen Hopkins" height="200" width="200" id="hopkins"/>
                     <a href="https://github.com/RaidoFive"> Stephen Hopkins </a>
-                    <span className="hopkins"> Stephen was a member of the may 2017 coding boot camp cohort. he has a passion for video game design and is currently seeking employment. </span>
+                    <span className="hopkins"> Stephen's favorite programming languages include Javascript and Jquery. He loves to read, especially works from Jim Busher. </span>
                 </div>
 
                 <div className="bui">
                     <img src="https://avatars2.githubusercontent.com/u/18633557?s=460&v=4" alt="Kevin Bui" height="200" width="200" id="bui"/>
                     <a href="https://github.com/buikevin96"> Kevin Bui </a>
-                    <span className="bui"> Kevin was a member of the may 2017 coding boot camp cohort. he has a passion for video game design and is currently seeking employment. </span>
+                    <span className="bui"> Kevin is a computer science major and likes to work with every language. His favorite hobbies include coding, working out, and reading. </span>
                 </div>
             </div>
         </div>
